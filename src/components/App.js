4885ffb350bdbd58c590cb0cc6002a551d0d22f8
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2021-08-26 11:43:55 -0400

    Implemented search feature and filetering

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Header from './Header';
 import LinkList from './LinkList';
 import { Switch, Route } from 'react-router-dom';
 import Login from './Login';
-
+import Search from './Search';
 
 const App = () => {
   return (
@@ -19,6 +19,7 @@ const App = () => {
             component={CreateLink}
           />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/search" component={Search} />
         </Switch>
       </div>
     </div>
